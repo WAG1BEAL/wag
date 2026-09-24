fc3f7718ed125445984cb08633ba679ed3e54c86
WAG1 <deranatac09@gmail.com>
2026-09-24 22:37:56 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Google Apps Script Web App URL'nı buraya yapıştır.
 // Örnek: https://script.google.com/macros/s/AKfycb.../exec
-window.WAG1_GAS_URL = "BURAYA_APPS_SCRIPT_WEB_APP_URL";
+window.WAG1_GAS_URL = "https://script.google.com/macros/s/AKfycby9VZy_ub6y_59ZLyahTguNq2rBTmVuWW6Al8ZJy3slvFL8x7aXxwhlQ275aONTXbs2Aw/exec";
