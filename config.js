1c555437b87c97de9ab80b2a00b7c27d46dcaa9e
WAG1 <deranatac09@gmail.com>
2026-09-25 13:08:27 +0300

    Update Google Apps Script Web App URL

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
-// Google Apps Script Web App URL'nı buraya yapıştır.
-// Örnek: https://script.google.com/macros/s/AKfycb.../exec
+
 window.WAG1_GAS_URL = "https://script.google.com/macros/s/AKfycby9VZy_ub6y_59ZLyahTguNq2rBTmVuWW6Al8ZJy3slvFL8x7aXxwhlQ275aONTXbs2Aw/exec";
